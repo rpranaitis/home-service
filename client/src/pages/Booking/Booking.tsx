ad54f09894ce1a92af8274bbc36b596afdf5a670
Andrew N <andzejus.naimovicius@gmail.com>
2024-07-11 21:32:12 +0300

    @ path used for eg ../../

---
+++ client/src/pages/Booking/Booking.tsx
@@ -0,0 +1,15 @@
+import style from './Booking.module.scss';
+
+import Hero from '@/components/Hero/Hero';
+import Categories from '@/components/Categories/Categories';
+
+const Booking = () => {
+  return (
+    <>
+      <Hero />
+      <Categories />
+    </>
+  );
+};
+
+export default Booking;
