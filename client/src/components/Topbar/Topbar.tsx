a05547604818294957d95be149a12ba17d0a6c60
Andrew N <andzejus.naimovicius@gmail.com>
2024-07-12 15:17:53 +0300

    Changes by code review by Rokas

--- client/src/components/Topbar/Topbar.tsx
+++ client/src/components/Topbar/Topbar.tsx
@@ -1,20 +1,11 @@
-import * as React from 'react';
+import React from 'react';
 import Logo from '../../assets/logo.svg';
 import styles from './Topbar.module.scss';
 import Button from '../Button/Button';
-import Avatar from '../Avatar/Avatar';
-import AvatarMaterialUi from '@mui/material/Avatar';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { ROUTES } from '../../router/constants';
 import { useUserContext } from '../../context/UserContext';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
+import AccountMenu from '../AccountMenu/AccountMenu';
 
 const links = [
   {
@@ -33,21 +24,7 @@ const links = [
 
 const Topbar = () => {
   const navigate = useNavigate();
-  const { user, logout } = useUserContext();
-
-  const handleLogout = () => {
-    logout();
-    navigate(ROUTES.HOME);
-  };
-
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  const { user } = useUserContext();
 
   return (
     <div className={styles.topbar}>
@@ -70,73 +47,7 @@ const Topbar = () => {
       <div className={styles.rightSide}>
         {user ? (
           <div className={styles.rightSideWrapper}>
-            <Tooltip title="Account settings">
-              <IconButton
-                onClick={handleClick}
-                size="small"
-                sx={{ ml: 2 }}
-                aria-controls={open ? 'account-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
-              >
-                <Avatar style={{ padding: '20px 20px', fontSize: '1.5rem' }}>{user.email[0]}</Avatar>
-              </IconButton>
-            </Tooltip>
-            <Menu
-              anchorEl={anchorEl}
-              id="account-menu"
-              open={open}
-              onClose={handleClose}
-              onClick={handleClose}
-              PaperProps={{
-                elevation: 0,
-                sx: {
-                  overflow: 'visible',
-                  filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                  mt: 1.5,
-                  '& .MuiAvatar-root': {
-                    width: 32,
-                    height: 32,
-                    ml: -0.5,
-                    mr: 1,
-                  },
-                  '&::before': {
-                    content: '""',
-                    display: 'block',
-                    position: 'absolute',
-                    top: 0,
-                    right: 21,
-                    width: 10,
-                    height: 10,
-                    bgcolor: 'background.paper',
-                    transform: 'translateY(-50%) rotate(45deg)',
-                    zIndex: 0,
-                  },
-                  '& .MuiMenuItem-root': {
-                    padding: '0.5rem 1rem',
-                  },
-                },
-              }}
-              transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-              anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-            >
-              <MenuItem onClick={handleClose}>
-                <AvatarMaterialUi /> My account
-              </MenuItem>
-              <Divider />
-              <MenuItem onClick={handleClose}>
-                <ListItemIcon>
-                  <Settings fontSize="small" />
-                </ListItemIcon>
-                My Booking
-              </MenuItem>
-              <MenuItem onClick={handleLogout}>
-                <ListItemIcon>
-                  <Logout fontSize="small" />
-                </ListItemIcon>
-                Logout
-              </MenuItem>
-            </Menu>
+            <AccountMenu user={user} />
           </div>
         ) : (
           <Button onClick={() => navigate(ROUTES.LOGIN)}>Login / Sign Up</Button>
