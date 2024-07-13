a22f7ba03b6ebd2b6fbdcea73f5b4d521fc4275d
rpranaitis <rolandaspranaitis@gmail.com>
2024-07-13 21:47:17 +0300

    Finished with Services page

--- client/src/pages/Services/Services.tsx
+++ client/src/pages/Services/Services.tsx
@@ -1,5 +1,22 @@
+import styles from './Services.module.scss';
+import { useBusinesses } from '../../hooks/business';
+import BusinessCard from '../../components/BusinessCard/BusinessCard';
+
 const Services = () => {
-  return <div>Services</div>;
+  const { data } = useBusinesses();
+
+  return (
+    <div className={styles.servicesContainer}>
+      <header className={styles.servicesHeader}>
+        <h1>Services</h1>
+      </header>
+      <div className={styles.servicesWrapper}>
+        {data?.map((item) => (
+          <BusinessCard key={item._id} business={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Services;
